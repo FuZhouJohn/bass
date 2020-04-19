3fac16640d84ef3315a588f975d6cb81aa007451
庄骥南 <a492073467@gmail.com>
2020-04-19 21:57:00 +0800

    使用 Vue 单文件组件

---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="b-button">按钮</button>
+</template>
+<scritp>
+    export default {
+
+    }
+</scritp>
+
+<style lang="scss">
+    .b-button {
+        height: var(--button-height);
+        padding: 0 1em;
+        font: inherit;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
