7d9241f3818da480f0705242fca068de91edd47c
庄骥南 <a492073467@gmail.com>
2020-04-20 22:29:36 +0800

    使用 chai.expect 添加测试用例，测试 icon、loading、iconPosition 效果

--- src/App.js
+++ src/App.js
@@ -15,3 +15,95 @@ new Vue({
         loading3: false
     }
 })
+
+// 单元测试
+import chai from 'chai'
+
+const expect = chai.expect;
+
+{
+    // icon
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'download'
+        }
+    })
+    vm.$mount()
+
+    const useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-download')
+
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    // loading
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'download',
+            loading: true
+        }
+    })
+    vm.$mount()
+    const useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
+
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    // icon-position default
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'download',
+        }
+    })
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    vm.$mount(div)
+    const svgElement = vm.$el.querySelector('svg')
+    let svgOrder = window.getComputedStyle(svgElement).order
+    expect(svgOrder).to.eq('1')
+
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    // icon-position default
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'download',
+            iconPosition: 'right'
+        }
+    })
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    vm.$mount(div)
+    const svgElement = vm.$el.querySelector('svg')
+    let svgOrder = window.getComputedStyle(svgElement).order
+    expect(svgOrder).to.eq('2')
+
+    vm.$el.remove()
+    vm.$destroy()
+
+}
+
+{
+    // 监听 click
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'download',
+            loading: true
+        }
+    })
+    vm.$mount()
+    vm.$on('click', function () {
+    })
+    const button = vm.$el
+    button.click()
+}
