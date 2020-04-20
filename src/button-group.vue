e3e8695b5d2ab60a6f02935a6c76efb1756c950c
庄骥南 <a492073467@gmail.com>
2020-04-20 20:09:26 +0800

    格式化

--- src/button-group.vue
+++ src/button-group.vue
@@ -9,7 +9,7 @@
         name: "button-group",
         mounted() {
             for (const node of this.$el.children) {
-                if(node.nodeName.toLowerCase() !== 'button'){
+                if (node.nodeName.toLowerCase() !== 'button') {
                     console.warn('b-button-group 的子元素只能是 b-button')
                 }
             }
@@ -19,32 +19,19 @@
 
 <style lang="scss">
     .b-button-group {
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        vertical-align: middle;
+        display: inline-flex;justify-content: center;align-items: center;vertical-align: middle;
 
         > .b-button {
             border-radius: 0;
 
-            & + .b-button {
-                margin-left: -1px;
-            }
+            & + .b-button {margin-left: -1px}
 
-            &:first-of-type {
-                border-top-left-radius: var(--border-radius);
-                border-bottom-left-radius: var(--border-radius);
-            }
+            &:first-of-type {border-top-left-radius: var(--border-radius);border-bottom-left-radius: var(--border-radius);}
 
-            &:last-of-type {
-                border-top-right-radius: var(--border-radius);
-                border-bottom-right-radius: var(--border-radius);
-            }
+            &:last-of-type {border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius)}
 
-            &:hover {
-                position: relative;
-                z-index: 1;
-            }
+            &:hover {position: relative;z-index: 1}
 
 
         }
