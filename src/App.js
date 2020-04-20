4abe6ed962402bc3496b7605f3c399156f9c94a0
庄骥南 <a492073467@gmail.com>
2020-04-20 20:05:30 +0800

    添加 button-group,并且检查子元素是否为 button 组件

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
+
 Vue.component('b-button', Button)
 Vue.component('b-icon', Icon)
+Vue.component('b-button-group', ButtonGroup)
+
 new Vue({
     el: '#app',
-    data:{
-        loading1:false,
-        loading2:false,
-        loading3:false
+    data: {
+        loading1: false,
+        loading2: false,
+        loading3: false
     }
 })
